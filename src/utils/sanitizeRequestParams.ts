9ec93122e2b558c362d3fdeba3a5203556f8c703
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2021-11-24 18:08:00 +0200

    fix(sanitize): We now properly sanitize boolean values

--- src/utils/sanitizeRequestParams.ts
+++ src/utils/sanitizeRequestParams.ts
@@ -3,6 +3,9 @@ export const sanitizeRequestParams = (obj: any) => {
   if (keys.length) {
     keys.forEach((key) => {
       if (!obj[key] && obj[key] !== 0) {
+        if (typeof obj[key] === 'boolean' && obj[key] !== null) {
+          return;
+        }
         delete obj[key];
       }
     });
