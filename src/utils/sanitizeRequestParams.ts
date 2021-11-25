5a22296458b87982d5de878603365827fdc3e761
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2021-11-25 12:35:46 +0200

    fix(sanitization): cleaner way to distinct legit boolean values and exclude them

--- src/utils/sanitizeRequestParams.ts
+++ src/utils/sanitizeRequestParams.ts
@@ -2,10 +2,7 @@ export const sanitizeRequestParams = (obj: any) => {
   const keys = Object.keys(obj);
   if (keys.length) {
     keys.forEach((key) => {
-      if (!obj[key] && obj[key] !== 0) {
-        if (typeof obj[key] === 'boolean' && obj[key] !== null) {
-          return;
-        }
+      if (!obj[key] && obj[key] !== 0 && !(typeof obj[key] === 'boolean')) {
         delete obj[key];
       }
     });
