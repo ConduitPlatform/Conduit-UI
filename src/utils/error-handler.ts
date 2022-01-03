d27b673eeb3c93fa989112ea969a6160fd9114e0
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-01-03 16:58:21 +0200

    fix(error-handler): added another case for fetching response 'message'

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -3,15 +3,18 @@ export const getErrorData = (errorProp: any) => {
     return;
   }
 
-  const { details } = errorProp.data;
+  const { details, message } = errorProp.data;
   const { error, status } = errorProp;
 
   const detailsFound = details !== '' && details !== null && details !== undefined;
   const errorFound = error !== '' && error !== null && error !== undefined;
   const statusFound = status !== '' && status !== null && status !== undefined;
+  const messageFound = message !== '' && message !== null && message !== undefined;
 
-  if (!detailsFound && !errorFound && !statusFound) {
+  if (!detailsFound && !errorFound && !statusFound && !messageFound) {
     return 'An unexpected error happened!';
   } else
-    return `${detailsFound ? details : ''} ${errorFound ? error : ''} ${statusFound ? status : ''}`;
+    return `${detailsFound ? details : ''} ${errorFound ? error : ''} ${
+      messageFound ? message : ''
+    } ${statusFound ? status : ''}`;
 };
