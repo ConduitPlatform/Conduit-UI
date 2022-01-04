c36092aec1c78defd48aa9af09421dafb85cbffd
jasonkatsoutas <jasonskate33@gmail.com>
2022-01-04 17:01:31 +0200

    fix: password slider no pointer events

--- src/components/common/FormComponents/PasswordStrengthMeter.tsx
+++ src/components/common/FormComponents/PasswordStrengthMeter.tsx
@@ -1,10 +1,11 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, memo } from 'react';
 import { Box, Slider, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { getPasswordStrength } from '../../../utils/getPasswordStrength';
 
 const useStyles = makeStyles(() => ({
   root: {
+    pointerEvents: 'none',
     color: (props: { colorValue: string }) => props.colorValue,
     '& .MuiSlider-thumb': {
       display: 'none',
@@ -23,13 +24,14 @@ interface PasswordStrengthProps {
 }
 
 const PasswordStrengthMeter: FC<PasswordStrengthProps> = ({ password }) => {
-  const passwordState = useMemo(() => {
+  const preparePasswordState = () => {
     const strength = getPasswordStrength(password);
     if (strength == 1) return { color: 'orangered', percentage: 30, message: 'password is weak' };
     if (strength == 2) return { color: '#8FCA11', percentage: 70, message: 'password is moderate' };
     if (strength == 3) return { color: 'green', percentage: 100, message: 'password is strong' };
     return { color: 'red', percentage: 0, message: 'password is very weak' };
-  }, [password]);
+  };
+  const passwordState = preparePasswordState();
 
   const classes = useStyles({ colorValue: passwordState.color });
 
@@ -43,4 +45,4 @@ const PasswordStrengthMeter: FC<PasswordStrengthProps> = ({ password }) => {
   );
 };
 
-export default PasswordStrengthMeter;
+export default memo(PasswordStrengthMeter);
