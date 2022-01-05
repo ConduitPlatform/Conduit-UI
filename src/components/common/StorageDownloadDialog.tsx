e9ce27d3de24b1454d6740c4268d86a2cbc66063
jasonkatsoutas <jasonskate33@gmail.com>
2022-01-05 16:14:04 +0200

    feat: storage download dialog loading on image preview

--- src/components/common/StorageDownloadDialog.tsx
+++ src/components/common/StorageDownloadDialog.tsx
@@ -1,11 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import {
+  Box,
   Button,
+  CircularProgress,
   Dialog,
   DialogActions,
   DialogContent,
   DialogProps,
   DialogTitle,
+  Grid,
   Typography,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -31,6 +34,11 @@ const StorageDownloadDialog: FC<DownloadDialogProps & DialogProps> = ({
   ...props
 }) => {
   const classes = useStyles();
+  const [imgLoading, setImgLoading] = useState(false);
+
+  useEffect(() => {
+    if (fileMimeType?.includes('image')) setImgLoading(true);
+  }, [fileMimeType, fileUrl]);
 
   const onClose = (e: React.MouseEvent<HTMLElement>) => {
     if (props?.onClose) props.onClose(e, 'backdropClick');
@@ -48,15 +56,27 @@ const StorageDownloadDialog: FC<DownloadDialogProps & DialogProps> = ({
             width={'100%'}
             height={'100%'}
             src={fileUrl}
+            onLoad={() => setImgLoading(false)}
             alt={'No available preview'}
           />
         ) : (
-          <Typography>No available preview</Typography>
+          <Box mb={1}>
+            <Typography>No available preview</Typography>
+          </Box>
         )}
+        {imgLoading ? (
+          <Grid container alignItems={'center'} justify={'center'}>
+            <Box mb={3}>
+              <CircularProgress />
+            </Box>
+          </Grid>
+        ) : undefined}
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>Cancel</Button>
-        <Button href={fileUrl}>Download</Button>
+        <Button variant={'outlined'} color={'secondary'} href={fileUrl}>
+          Download
+        </Button>
       </DialogActions>
     </Dialog>
   );
