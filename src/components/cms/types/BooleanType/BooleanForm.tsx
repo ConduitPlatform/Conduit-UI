a08deab6529a166bfcf73337ad3bba8be742d66e
jasonkatsoutas <jasonskate33@gmail.com>
2022-01-12 14:30:39 +0200

    fix: Build Types Boolean Form with useForm

--- src/components/cms/types/BooleanType/BooleanForm.tsx
+++ src/components/cms/types/BooleanType/BooleanForm.tsx
@@ -1,30 +1,22 @@
-import React, { FC, MouseEventHandler, useState } from 'react';
+import React, { FC, MouseEventHandler, useMemo } from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import Switch from '@material-ui/core/Switch';
-import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { IBooleanData, IDrawerData } from '../../../../models/cms/BuildTypesModels';
+import { FormProvider, useForm } from 'react-hook-form';
+import { FormInputText } from '../../../common/FormComponents/FormInputText';
+import { FormInputSwitch } from '../../../common/FormComponents/FormInputSwitch';
+import { FormInputCheckBox } from '../../../common/FormComponents/FormInputCheckbox';
 
 const useStyles = makeStyles((theme) => ({
-  textField: {
-    marginBottom: theme.spacing(1),
-  },
   info: {
-    width: '100%',
-    fontSize: 14,
-    marginBottom: theme.spacing(3),
     opacity: '0.5',
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '100%',
+    display: 'grid',
+    gap: theme.spacing(2),
     padding: theme.spacing(2),
   },
 }));
@@ -58,256 +50,128 @@ const BooleanForm: FC<IProps> = ({
 }) => {
   const classes = useStyles();
 
-  const [booleanData, setBooleanData] = useState({
-    name: selectedItem ? selectedItem.name : '',
-    placeholderFalse: selectedItem ? selectedItem.placeholderFalse : '',
-    placeholderTrue: selectedItem ? selectedItem.placeholderTrue : '',
-    type: selectedItem ? selectedItem.type : drawerData.type,
-    default: selectedItem ? selectedItem.default : false,
-    unique: selectedItem ? selectedItem.unique : false,
-    select: selectedItem ? selectedItem.select : true,
-    required: selectedItem ? selectedItem.required : false,
-    isArray: selectedItem ? selectedItem.isArray : false,
-    // id: '',
+  const methods = useForm({
+    defaultValues: useMemo(() => {
+      return {
+        name: selectedItem ? selectedItem.name : '',
+        placeholderFalse: selectedItem ? selectedItem.placeholderFalse : '',
+        placeholderTrue: selectedItem ? selectedItem.placeholderTrue : '',
+        type: selectedItem ? selectedItem.type : drawerData.type,
+        default: selectedItem ? selectedItem.default : false,
+        unique: selectedItem ? selectedItem.unique : false,
+        select: selectedItem ? selectedItem.select : true,
+        required: selectedItem ? selectedItem.required : false,
+        isArray: selectedItem ? selectedItem.isArray : false,
+      };
+    }, [selectedItem, drawerData]),
   });
+  const defaultValue = methods.watch('default');
 
-  const handleFieldName = (event: { target: { value: string } }) => {
-    // const slug = slugify(event.target.value);
-    setBooleanData({
-      ...booleanData,
-      name: event.target.value.split(' ').join(''),
-      // id: slug,
-    });
-  };
-
-  const handleFalsePlaceholder = (event: { target: { value: string } }) => {
-    setBooleanData({ ...booleanData, placeholderFalse: event.target.value });
-  };
-
-  const handleTruePlaceholder = (event: { target: { value: string } }) => {
-    setBooleanData({ ...booleanData, placeholderTrue: event.target.value });
-  };
-
-  const handleFieldDefault = () => {
-    setBooleanData({ ...booleanData, default: !booleanData.default });
-  };
-
-  const handleFieldUnique = () => {
-    setBooleanData({ ...booleanData, unique: !booleanData.unique });
-  };
-
-  const handleFieldRequired = () => {
-    setBooleanData({ ...booleanData, required: !booleanData.required });
-  };
-
-  const handleFieldSelect = () => {
-    setBooleanData({ ...booleanData, select: !booleanData.select });
-  };
-
-  const handleFieldIsArray = () => {
-    setBooleanData({ ...booleanData, isArray: !booleanData.isArray });
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    onSubmit(booleanData);
-    event.preventDefault();
+  const handleSubmit = (data: any) => {
+    onSubmit(data);
   };
 
   return (
-    <form autoComplete="off" onSubmit={handleSubmit} className={classes.form} {...rest}>
-      <TextField
-        id="Field Name"
-        label="Field Name"
-        onChange={handleFieldName}
-        value={booleanData.name}
-        variant="outlined"
-        className={classes.textField}
-        fullWidth
-        required
-        InputProps={{
-          readOnly: readOnly && !!selectedItem,
-        }}
-        helperText={'This is the name of the field in the schema model'}
-      />
-      <TextField
-        id="False Placeholder"
-        label="False Placeholder"
-        onChange={handleFalsePlaceholder}
-        placeholder={'false'}
-        value={booleanData.placeholderFalse}
-        variant="outlined"
-        className={classes.textField}
-        fullWidth
-        required
-        helperText={'Placeholder to appear in the editor'}
-      />
-      <TextField
-        id="True Placeholder"
-        label="True Placeholder"
-        onChange={handleTruePlaceholder}
-        placeholder={'true'}
-        value={booleanData.placeholderTrue}
-        variant="outlined"
-        className={classes.textField}
-        fullWidth
-        required
-        helperText={'Placeholder to appear in the editor'}
-      />
-      <Box width={'100%'}>
-        <Grid container>
-          <Grid item xs={12}>
-            <Box
-              width={'100%'}
-              display={'inline-flex'}
-              justifyContent={'space-between'}
-              alignItems={'center'}>
-              <Typography variant={'button'} style={{ width: '100%' }}>
-                Default Value
-              </Typography>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={booleanData.default}
-                    onChange={handleFieldDefault}
-                    color="primary"
-                  />
-                }
-                label={booleanData.default ? 'True' : 'False'}
-              />
+    <FormProvider {...methods}>
+      <form
+        autoComplete="off"
+        onSubmit={methods.handleSubmit(handleSubmit)}
+        className={classes.form}
+        {...rest}>
+        <Box>
+          <FormInputText
+            textFieldProps={{
+              InputProps: { readOnly: readOnly && !!selectedItem },
+            }}
+            rules={{
+              required: 'Name is required',
+              pattern: { value: /^\S*$/, message: 'No whitespace allowed' },
+            }}
+            label={'field Name'}
+            name={'name'}
+          />
+          <Typography variant={'subtitle2'} className={classes.info}>
+            This is the name of the field in the schema model
+          </Typography>
+        </Box>
+        <Box>
+          <FormInputText
+            rules={{ required: 'Placeholder is required' }}
+            label={'False Placeholder'}
+            name={'placeholderFalse'}
+          />
+          <Typography variant={'subtitle2'} className={classes.info}>
+            Placeholder to appear in the editor
+          </Typography>
+        </Box>
+        <Box>
+          <FormInputText
+            rules={{ required: 'Placeholder is required' }}
+            label={'True Placeholder'}
+            name={'placeholderTrue'}
+          />
+          <Typography variant={'subtitle2'} className={classes.info}>
+            Placeholder to appear in the editor
+          </Typography>
+        </Box>
+        <Box>
+          <Grid container justifyContent={'space-between'} alignItems={'center'}>
+            <Typography>Default Value</Typography>
+            <Box display={'flex'} alignItems={'center'}>
+              <FormInputSwitch name={'default'} />
+              <Typography>{defaultValue ? 'True' : 'False'}</Typography>
             </Box>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant={'subtitle1'} className={classes.info}>
-              The default value of the field
-            </Typography>
+          <Typography variant={'subtitle2'} className={classes.info}>
+            The default value of the field
+          </Typography>
+        </Box>
+        <Box>
+          <Grid container justifyContent={'space-between'} alignItems={'center'}>
+            <Typography>Unique field</Typography>
+            <FormInputSwitch name={'unique'} />
           </Grid>
-        </Grid>
-
-        <Grid container>
-          <Grid item xs={12}>
-            <Box
-              width={'100%'}
-              display={'inline-flex'}
-              justifyContent={'space-between'}
-              alignItems={'center'}>
-              <Typography variant={'button'} style={{ width: '100%' }}>
-                Unique field
-              </Typography>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={booleanData.unique}
-                    onChange={handleFieldUnique}
-                    color="primary"
-                  />
-                }
-                label=""
-              />
-            </Box>
+          <Typography variant={'subtitle2'} className={classes.info}>
+            {"If active, this field's value must be unique"}
+          </Typography>
+        </Box>
+        <Box>
+          <Grid container justifyContent={'space-between'} alignItems={'center'}>
+            <Typography>Required</Typography>
+            <FormInputSwitch name={'required'} />
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant={'subtitle1'} className={classes.info}>
-              {"If active, this field's value must be unique"}
-            </Typography>
+          <Typography variant={'subtitle2'} className={classes.info}>
+            If active, this field will be required
+          </Typography>
+        </Box>
+        <Box>
+          <Grid container justifyContent={'space-between'} alignItems={'center'}>
+            <Typography>Select</Typography>
+            <FormInputSwitch name={'select'} />
           </Grid>
-        </Grid>
-
-        <Grid container>
-          <Grid item xs={12}>
-            <Box
-              width={'100%'}
-              display={'inline-flex'}
-              justifyContent={'space-between'}
-              alignItems={'center'}>
-              <Typography variant={'button'} style={{ width: '100%' }}>
-                Required
-              </Typography>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={booleanData.required}
-                    onChange={handleFieldRequired}
-                    color="primary"
-                  />
-                }
-                label=""
-              />
-            </Box>
+          <Typography variant={'subtitle2'} className={classes.info}>
+            This option defines if the field you be returned from the database
+          </Typography>
+        </Box>
+        <Box>
+          <Grid container justifyContent={'space-between'} alignItems={'center'}>
+            <Typography>Array</Typography>
+            <FormInputCheckBox name={'isArray'} label={''} />
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant={'body2'} className={classes.info}>
-              If active, this field will be required
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid container>
-          <Grid item xs={12}>
-            <Box
-              width={'100%'}
-              display={'inline-flex'}
-              justifyContent={'space-between'}
-              alignItems={'center'}>
-              <Typography variant={'button'} style={{ width: '100%' }}>
-                Select
-              </Typography>
-              <FormControlLabel
-                control={
-                  <Switch
-                    checked={booleanData.select}
-                    onChange={handleFieldSelect}
-                    color="primary"
-                  />
-                }
-                label=""
-              />
-            </Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant={'body2'} className={classes.info}>
-              This option defines if the field you be returned from the database
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid container>
-          <Grid item xs={12}>
-            <Box
-              width={'100%'}
-              display={'inline-flex'}
-              justifyContent={'space-between'}
-              alignItems={'center'}>
-              <Typography variant={'button'} style={{ width: '100%' }}>
-                Array
-              </Typography>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={booleanData.isArray}
-                    onChange={handleFieldIsArray}
-                    color="primary"
-                  />
-                }
-                label=""
-              />
-            </Box>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant={'body2'} className={classes.info}>
-              Activate this option if you want your field to be of type Array
-            </Typography>
-          </Grid>
-        </Grid>
-      </Box>
-      <Box display={'flex'} width={'100%'}>
-        <Button variant="contained" color="primary" type="submit" style={{ marginRight: 16 }}>
-          OK
-        </Button>
-        <Button variant="contained" onClick={onClose}>
-          CANCEL
-        </Button>
-      </Box>
-    </form>
+          <Typography variant={'subtitle2'} className={classes.info}>
+            Activate this option if you want your field to be of type Array
+          </Typography>
+        </Box>
+        <Box display={'flex'} width={'100%'}>
+          <Button variant="contained" color="primary" type="submit" style={{ marginRight: 16 }}>
+            OK
+          </Button>
+          <Button variant="contained" onClick={onClose}>
+            CANCEL
+          </Button>
+        </Box>
+      </form>
+    </FormProvider>
   );
 };
 
