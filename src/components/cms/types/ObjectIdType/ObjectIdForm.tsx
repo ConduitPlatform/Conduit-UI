86845bfc878d91b5ba8bcfdc9157efbc86272ea1
Konstantinos Fegoulis <56806349+KostasFeg@users.noreply.github.com>
2022-01-20 12:12:36 +0200

    fix(typos): typo leftovers ffixed

--- src/components/cms/types/ObjectIdType/ObjectIdForm.tsx
+++ src/components/cms/types/ObjectIdType/ObjectIdForm.tsx
@@ -174,7 +174,7 @@ const ObjectIdForm: FC<IProps> = ({
           </Grid>
           <Grid item xs={12}>
             <Typography variant={'body2'} className={classes.info}>
-              This option defines if the field you be returned from the database
+              This option defines if the field should be returned from the database
             </Typography>
           </Grid>
         </Grid>
