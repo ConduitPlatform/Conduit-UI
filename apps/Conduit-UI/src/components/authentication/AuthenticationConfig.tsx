fdca8ce34bad7a9f8537521423a4df6f797cc410
kkopanidis <kkopanidis@gmail.com>
2022-05-09 19:48:15 +0300

    feat(conduit-ui): new authentication module config options

--- apps/Conduit-UI/src/components/authentication/AuthenticationConfig.tsx
+++ apps/Conduit-UI/src/components/authentication/AuthenticationConfig.tsx
@@ -34,6 +34,11 @@ const AuthenticationConfig: React.FC = () => {
     name: 'active',
   });
 
+  const useCookies = useWatch({
+    control,
+    name: 'setCookies.enabled',
+  });
+
   const handleCancel = () => {
     setEdit(!edit);
     methods.reset();
@@ -87,12 +92,137 @@ const AuthenticationConfig: React.FC = () => {
                       display={'inline-flex'}
                       justifyContent={'space-between'}
                       alignItems={'center'}>
-                      <Typography variant={'subtitle1'}>Allow Refresh Token generation</Typography>
+                      <Typography variant={'subtitle1'}>Generate Refresh Tokens</Typography>
                       <FormInputSwitch name={'generateRefreshToken'} disabled={!edit} />
+                    </Box>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Box
+                      width={'100%'}
+                      display={'inline-flex'}
+                      justifyContent={'space-between'}
+                      alignItems={'center'}>
+                      <Typography variant={'subtitle1'}>
+                        Multiple User Sessions per Client
+                      </Typography>
+                      <FormInputSwitch name={'clients.multipleUserSessions'} disabled={!edit} />
+                    </Box>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Box
+                      width={'100%'}
+                      display={'inline-flex'}
+                      justifyContent={'space-between'}
+                      alignItems={'center'}>
+                      <Typography variant={'subtitle1'}>
+                        User can be logged in to multiple Clients
+                      </Typography>
+                      <FormInputSwitch name={'clients.multipleClientLogins'} disabled={!edit} />
                     </Box>
                   </Grid>
                 </>
               )}
+            </Grid>
+
+            <Divider sx={{ mt: 2, mb: 2, width: '100%' }} />
+            <Grid container>
+              <Box
+                width={'100%'}
+                display={'inline-flex'}
+                justifyContent={'space-between'}
+                alignItems={'center'}>
+                <Typography variant={'h6'}>Use cookies</Typography>
+                <FormInputSwitch name={'setCookies.enabled'} disabled={!edit} />
+              </Box>
+              <Grid container spacing={5} sx={{ padding: 3 }}>
+                {useCookies && (
+                  <>
+                    <Grid key={0} item xs={6}>
+                      <Box
+                        width={'100%'}
+                        display={'inline-flex'}
+                        justifyContent={'space-between'}
+                        alignItems={'center'}>
+                        <Typography variant={'subtitle1'}>httpOnly</Typography>
+                        <FormInputSwitch name={'setCookies.httpOnly'} disabled={!edit} />
+                      </Box>
+                    </Grid>
+                    <Grid key={1} item xs={6}>
+                      <Box
+                        width={'100%'}
+                        display={'inline-flex'}
+                        justifyContent={'space-between'}
+                        alignItems={'center'}>
+                        <Typography variant={'subtitle1'}>secure</Typography>
+                        <FormInputSwitch name={'setCookies.secure'} disabled={!edit} />
+                      </Box>
+                    </Grid>
+                    <Grid key={2} item xs={6}>
+                      <Box
+                        width={'100%'}
+                        display={'inline-flex'}
+                        justifyContent={'space-between'}
+                        alignItems={'center'}>
+                        <Typography variant={'subtitle1'}>signed</Typography>
+                        <FormInputSwitch name={'setCookies.signed'} disabled={!edit} />
+                      </Box>
+                    </Grid>
+                    <Grid key={3} item xs={6}>
+                      <Box
+                        width={'100%'}
+                        display={'inline-flex'}
+                        justifyContent={'space-between'}
+                        alignItems={'center'}>
+                        <FormInputText
+                          typeOfInput={'number'}
+                          name={'setCookies.maxAge'}
+                          label={startCase(camelCase('maxAge'))}
+                          disabled={!edit}
+                        />
+                      </Box>
+                    </Grid>
+                    <Grid key={4} item xs={6}>
+                      <Box
+                        width={'100%'}
+                        display={'inline-flex'}
+                        justifyContent={'space-between'}
+                        alignItems={'center'}>
+                        <FormInputText
+                          name={'setCookies.domain'}
+                          label={startCase(camelCase('domain'))}
+                          disabled={!edit}
+                        />
+                      </Box>
+                    </Grid>
+                    <Grid key={5} item xs={6}>
+                      <Box
+                        width={'100%'}
+                        display={'inline-flex'}
+                        justifyContent={'space-between'}
+                        alignItems={'center'}>
+                        <FormInputText
+                          name={'setCookies.path'}
+                          label={startCase(camelCase('path'))}
+                          disabled={!edit}
+                        />
+                      </Box>
+                    </Grid>
+                    <Grid key={6} item xs={6}>
+                      <Box
+                        width={'100%'}
+                        display={'inline-flex'}
+                        justifyContent={'space-between'}
+                        alignItems={'center'}>
+                        <FormInputText
+                          name={'setCookies.sameSite'}
+                          label={startCase(camelCase('sameSite'))}
+                          disabled={!edit}
+                        />
+                      </Box>
+                    </Grid>
+                  </>
+                )}
+              </Grid>
             </Grid>
             <ConfigSaveSection edit={edit} setEdit={setEdit} handleCancel={handleCancel} />
           </Grid>
