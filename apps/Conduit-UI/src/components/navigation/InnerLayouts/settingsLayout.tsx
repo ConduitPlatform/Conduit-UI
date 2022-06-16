636980a3be1ac02199c2722bda877fdfd1d42c8a
Nikolaos Tseperkas <35974416+N1tse@users.noreply.github.com>
2022-06-16 14:22:09 +0300

    refactor(settings): remove clients SDK and rename admin section to ad… (#91)

--- apps/Conduit-UI/src/components/navigation/InnerLayouts/settingsLayout.tsx
+++ apps/Conduit-UI/src/components/navigation/InnerLayouts/settingsLayout.tsx
@@ -3,16 +3,10 @@ import { Settings } from '@mui/icons-material';
 import StyledLayout from './styledLayout';
 
 const SettingsLayout: React.FC = ({ children }) => {
-  const pathNames = [
-    '/settings/clientsdk',
-    '/settings/core',
-    '/settings/admins',
-    '/settings/password',
-  ];
+  const pathNames = ['/settings/core', '/settings/admins', '/settings/password'];
   const labels = [
-    { name: 'clients SDK', id: 'clientsdk' },
     { name: 'core', id: 'core' },
-    { name: 'admin section', id: 'admins' },
+    { name: 'admin users', id: 'admins' },
     { name: 'change password', id: 'password' },
   ];
 
