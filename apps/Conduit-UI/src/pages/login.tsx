c2ed6147a506e49f107cb4035045d07bff015ca2
Voudromnianos Apostolos <69977309+AVoudromnianos@users.noreply.github.com>
2022-06-17 18:06:11 +0300

    fix(login): login page ui refactor (#95)

--- apps/Conduit-UI/src/pages/login.tsx
+++ apps/Conduit-UI/src/pages/login.tsx
@@ -3,7 +3,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { asyncLogin } from '../redux/slices/appAuthSlice';
-import { Box } from '@mui/material';
+import { Box, Fade, useMediaQuery } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../redux/store';
 import { useRouter } from 'next/router';
 import Grid from '@mui/material/Grid';
@@ -13,6 +13,7 @@ import { FormInputText } from '../components/common/FormComponents/FormInputText
 import { FormInputCheckBox } from '../components/common/FormComponents/FormInputCheckbox';
 import ConduitLogo from '../assets/svgs/conduitLogo.svg';
 import Image from 'next/image';
+import theme from '../theme';
 
 interface ILoginValues {
   username: string;
@@ -25,6 +26,7 @@ const Login: React.FC = () => {
   const { loading } = useAppSelector((state) => state.appSlice);
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const smallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const methods = useForm<ILoginValues>({
     defaultValues: { username: '', password: '', remember: false },
@@ -41,62 +43,90 @@ const Login: React.FC = () => {
   };
 
   return (
-    <Container maxWidth="xl">
-      <Grid container sx={{ height: '100vh' }}>
-        <Grid container item xs={8} sx={{ background: '#262840', p: 4 }}>
-          <LoginIllustration />
-        </Grid>
+    <Container
+      maxWidth={false}
+      sx={{ display: 'flex', flex: 1, overflow: 'hidden' }}
+      disableGutters>
+      {smallScreen ? null : (
         <Grid
           container
           item
-          xs={4}
           sx={{
-            flexDirection: 'column',
+            background: '#1c1d30',
             alignItems: 'center',
             justifyContent: 'center',
-            background: 'background.paper',
-          }}>
-          <Box marginBottom="20px">
-            <Image src={ConduitLogo} alt="conduit-logo" />
-          </Box>
-          <Typography variant="h6">Sign in</Typography>
-          <Container maxWidth="xs">
-            <FormProvider {...methods}>
-              <form onSubmit={methods.handleSubmit(handleLogin)} style={{ width: '100%' }}>
-                <Box mt={2}>
-                  <FormInputText
-                    name="username"
-                    rules={{
-                      required: 'Username is required',
-                    }}
-                    label="Username"
-                  />
-                </Box>
-                <Box mt={2}>
-                  <FormInputText
-                    typeOfInput="password"
-                    name="password"
-                    rules={{
-                      required: 'Password is required',
-                    }}
-                    label="Password"
-                  />
-                </Box>
-                <Box display="flex" justifyContent="flex-end">
-                  <FormInputCheckBox name="remember" label="Remember me" />
-                </Box>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  disabled={loading}>
-                  Sign In
-                </Button>
-              </form>
-            </FormProvider>
-          </Container>
+          }}
+          maxWidth={'md'}>
+          <LoginIllustration width={'90%'} />
         </Grid>
+      )}
+      <Grid
+        container
+        item
+        display={'flex'}
+        flexDirection={'column'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        marginX={2}>
+        <Fade timeout={1200} in>
+          <Grid container display={'flex'} alignItems={'center'} justifyContent={'center'}>
+            <Box marginBottom={5} display={'flex'}>
+              <Image src={ConduitLogo} alt="conduit-logo" width={300} height={80} priority />
+            </Box>
+            <Grid
+              container
+              item
+              flexDirection={'column'}
+              alignItems={'center'}
+              justifyContent={'center'}
+              sx={{
+                background: 'background.paper',
+              }}>
+              <Typography variant="h6">Sign in</Typography>
+              <Container maxWidth="xs">
+                <FormProvider {...methods}>
+                  <form onSubmit={methods.handleSubmit(handleLogin)}>
+                    <Box mt={2}>
+                      <FormInputText
+                        name="username"
+                        rules={{
+                          required: 'Username is required',
+                        }}
+                        label="Username"
+                      />
+                    </Box>
+                    <Box mt={2}>
+                      <FormInputText
+                        typeOfInput="password"
+                        name="password"
+                        rules={{
+                          required: 'Password is required',
+                        }}
+                        label="Password"
+                      />
+                    </Box>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                      }}>
+                      <FormInputCheckBox name="remember" label="Remember me" />
+                    </Box>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      disabled={loading}
+                      sx={{ whiteSpace: 'nowrap' }}>
+                      Sign In
+                    </Button>
+                  </form>
+                </FormProvider>
+              </Container>
+            </Grid>
+          </Grid>
+        </Fade>
       </Grid>
     </Container>
   );
