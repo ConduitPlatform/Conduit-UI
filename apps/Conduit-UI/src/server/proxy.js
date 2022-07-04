c39bd2bd167603fd2b9439f5946833ae1cf9a52a
kkopanidis <kkopanidis@gmail.com>
2022-07-04 17:05:59 +0300

    refactor: change proxy env to CONDUIT_URL

--- apps/Conduit-UI/src/server/proxy.js
+++ apps/Conduit-UI/src/server/proxy.js
@@ -5,7 +5,7 @@ export const proxy = httpProxy.createProxyServer({
    * Get the actual bac-end service url from env variables.
    * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
    */
-  target: process.env.SERVICE_URL || 'NULL',
+  target: process.env.CONDUIT_URL || 'NULL',
   changeOrigin: true,
   autoRewrite: false,
 });
