a59f5dbaa3d254bd60213cdbff005b91ca5393e4
Avoudromnianos <aposbou@gmail.com>
2022-07-12 16:17:26 +0300

    feat: chat lazy loading

--- apps/Conduit-UI/src/pages/chat/config.tsx
+++ apps/Conduit-UI/src/pages/chat/config.tsx
@@ -1,8 +1,15 @@
 import React, { ReactElement, useEffect } from 'react';
 import ChatLayout from '../../components/navigation/InnerLayouts/chatLayout';
-import ChatConfig from '../../components/chat/ChatConfig';
 import { asyncGetChatConfig } from '../../redux/slices/chatSlice';
 import { useAppDispatch } from '../../redux/store';
+import dynamic from 'next/dynamic';
+import ScaleLoader from 'react-spinners/ScaleLoader';
+
+const ChatConfig = dynamic(() => import('../../components/chat/ChatConfig'), {
+  loading: () => (
+    <ScaleLoader speedMultiplier={3} color={'#07D9C4'} loading={true} height={21} width={4} />
+  ),
+});
 
 const Config = () => {
   const dispatch = useAppDispatch();
