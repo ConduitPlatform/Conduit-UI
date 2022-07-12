eb97ba76e074b6f414f48789202c314c3de95f54
Avoudromnianos <aposbou@gmail.com>
2022-07-12 17:13:55 +0300

    feat: storage lazy loading

--- apps/Conduit-UI/src/pages/storage/files.tsx
+++ apps/Conduit-UI/src/pages/storage/files.tsx
@@ -1,6 +1,13 @@
 import React, { ReactElement } from 'react';
 import StorageLayout from '../../components/navigation/InnerLayouts/storageLayout';
-import StorageFiles from '../../components/storage/StorageFiles';
+import dynamic from 'next/dynamic';
+import ScaleLoader from 'react-spinners/ScaleLoader';
+
+const StorageFiles = dynamic(() => import('../../components/storage/StorageFiles'), {
+  loading: () => (
+    <ScaleLoader speedMultiplier={3} color={'#07D9C4'} loading={true} height={21} width={4} />
+  ),
+});
 
 const Files = () => {
   return <StorageFiles />;
