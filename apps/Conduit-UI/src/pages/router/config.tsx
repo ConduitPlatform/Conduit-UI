814aa6ffc3d02448fe655f0fa233a612f66801aa
Avoudromnianos <aposbou@gmail.com>
2022-07-12 17:01:49 +0300

    feat: router lazy loading

--- apps/Conduit-UI/src/pages/router/config.tsx
+++ apps/Conduit-UI/src/pages/router/config.tsx
@@ -1,8 +1,15 @@
 import React, { ReactElement, useEffect } from 'react';
 import { useAppDispatch } from '../../redux/store';
 import { asyncGetRouterConfig } from '../../redux/slices/routerSlice';
-import SecurityConfig from '../../components/routing/SecurityConfig';
 import RouterLayout from '../../components/navigation/InnerLayouts/routerLayout';
+import dynamic from 'next/dynamic';
+import ScaleLoader from 'react-spinners/ScaleLoader';
+
+const SecurityConfig = dynamic(() => import('../../components/routing/SecurityConfig'), {
+  loading: () => (
+    <ScaleLoader speedMultiplier={3} color={'#07D9C4'} loading={true} height={21} width={4} />
+  ),
+});
 
 const Config = () => {
   const dispatch = useAppDispatch();
--- apps/Conduit-UI/src/pages/router/settings.tsx
+++ apps/Conduit-UI/src/pages/router/settings.tsx
@@ -1,8 +1,15 @@
 import React, { ReactElement, useEffect } from 'react';
 import { useAppDispatch } from '../../redux/store';
 import RouterLayout from '../../components/navigation/InnerLayouts/routerLayout';
-import RouterSettings from '../../components/routing/routerSettings';
 import { asyncGetRouterConfig } from '../../redux/slices/routerSlice';
+import dynamic from 'next/dynamic';
+import ScaleLoader from 'react-spinners/ScaleLoader';
+
+const RouterSettings = dynamic(() => import('../../components/routing/routerSettings'), {
+  loading: () => (
+    <ScaleLoader speedMultiplier={3} color={'#07D9C4'} loading={true} height={21} width={4} />
+  ),
+});
 
 const Settings = () => {
   const dispatch = useAppDispatch();
