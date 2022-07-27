202ff406b34525727e7c1150ac1301748b86b522
spyrossellas <sellas.spy@gmail.com>
2022-07-27 14:22:20 +0300

    feat: loki proxy

--- apps/Conduit-UI/src/pages/api/[...path].ts
+++ apps/Conduit-UI/src/pages/api/[...path].ts
@@ -1,33 +1,41 @@
-import { proxy } from '../../server/proxy';
+import { proxy, proxyLoki } from '../../server/proxy';
 import http from 'http';
 
 const path = (req: http.IncomingMessage, res: http.ServerResponse) => {
   return new Promise((resolve, reject) => {
-    // removes the api prefix from url
-    req.url = req.url?.replace(/^\/api/, '');
+    if (req.url?.match(/^\/api\/loki/)) {
+      req.url = req.url?.replace(/^\/api\/loki/, '');
 
-    // add masterkey since the initial req does not include
-    // it for security measures
-    req.headers.masterkey = process.env.MASTER_KEY;
+      proxyLoki.once('error', reject);
 
-    // const cookies = new Cookies(req, res);
-    // const authorization = cookies.get('authorization');
-    //
-    // // don't forwards the cookies to the target server
-    // req.headers.cookie = '';
-    //
-    // if (authorization) {
-    //   req.headers.authorization = authorization;
-    // }
+      proxyLoki.web(req, res);
+    } else {
+      // removes the api prefix from url
+      req.url = req.url?.replace(/^\/api/, '');
 
-    /**
-     * if an error occurs in the proxy, we will reject the promise.
-     * it is so important. if you don't reject the promise,
-     * you're facing the stalled requests issue.
-     */
-    proxy.once('error', reject);
+      // add masterkey since the initial req does not include
+      // it for security measures
+      req.headers.masterkey = process.env.MASTER_KEY;
 
-    proxy.web(req, res);
+      // const cookies = new Cookies(req, res);
+      // const authorization = cookies.get('authorization');
+      //
+      // // don't forwards the cookies to the target server
+      // req.headers.cookie = '';
+      //
+      // if (authorization) {
+      //   req.headers.authorization = authorization;
+      // }
+
+      /**
+       * if an error occurs in the proxy, we will reject the promise.
+       * it is so important. if you don't reject the promise,
+       * you're facing the stalled requests issue.
+       */
+      proxy.once('error', reject);
+
+      proxy.web(req, res);
+    }
   });
 };
 export default path;
