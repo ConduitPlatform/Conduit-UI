28dadadac7f9e308ecd11fd38add7cccec9ef28d
Avoudromnianos <aposbou@gmail.com>
2022-08-04 19:25:34 +0300

    fix: logs component ui, functionality, styles

--- apps/Conduit-UI/src/components/logs/LogsComponent.tsx
+++ apps/Conduit-UI/src/components/logs/LogsComponent.tsx
@@ -19,212 +19,313 @@ import ListItemText from '@mui/material/ListItemText';
 import LogsList from './LogsList';
 import Paper from '@mui/material/Paper';
 import { useAppDispatch, useAppSelector } from '../../redux/store';
-import {
-  asyncGetAuthenticationLevels,
-  asyncGetAuthenticationQueryRange,
-} from '../../redux/slices/authenticationSlice';
-import moment, { MomentInput } from 'moment';
+import moment, { Moment } from 'moment';
 import RefreshIcon from '@mui/icons-material/Refresh';
-import { debounce } from 'lodash';
+import { debounce, throttle } from 'lodash';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  asyncGetInstances,
+  asyncGetLevels,
+  asyncGetQueryRange,
+} from '../../redux/slices/LogsSlice';
+import { ModulesTypes } from '../../models/logs/LogsModels';
 
-const LogsComponent: React.FC = () => {
+interface Props {
+  moduleName: ModulesTypes;
+}
+
+const Limits = [50, 100, 300, 500, 1000];
+
+const LogsComponent: React.FC<Props> = ({ moduleName }) => {
   const dispatch = useAppDispatch();
-  const logsLevels = useAppSelector((state) => state.authenticationSlice?.data?.logs.levels);
-  // const instances = useAppSelector((state) => state.authenticationSlice?.data?.logs.instances);
-  const values = useAppSelector((state) => state.authenticationSlice?.data?.logs.query);
-  const [selectedLevels, setSelectedLevels] = useState<any>(logsLevels);
-  // const [selectedInstances, setInstances] = useState<string[]>([]);
-  const [startDateValue, setStartDateValue] = useState<MomentInput | null>(
-    moment().subtract(1, 'hours')
-  );
-  const [endDateValue, setEndDateValue] = useState<MomentInput | null>(moment());
+  const logsLevels = useAppSelector((state) => state.logsSlice?.levels);
+  const instances = useAppSelector((state) => state.logsSlice?.instances);
+  const values = useAppSelector((state) => state.logsSlice?.logs?.[moduleName]);
 
-  const query = useMemo(() => {
-    const string = selectedLevels?.join('|');
-    return string ? `{module="authentication", level=~"${string}" }` : `{module="authentication"}`;
-  }, [selectedLevels]);
+  const [selectedLevels, setSelectedLevels] = useState<string[]>([]);
+  const [selectedInstances, setInstances] = useState<string[]>([]);
+  const [selectedLimit, setSelectedLimit] = useState<number>(100);
+  const [startDateValue, setStartDateValue] = useState<Moment | null>(null);
+  const [endDateValue, setEndDateValue] = useState<Moment | null>(null);
+  const [isStartDatePickerOpen, setIsStartDatePickerOpen] = useState<boolean>(false);
+  const [isEndDatePickerOpen, setIsEndDatePickerOpen] = useState<boolean>(false);
+
+  const requestDebounce = useCallback(
+    debounce(() => {
+      dispatch(
+        asyncGetQueryRange({
+          module: moduleName,
+          levels: selectedLevels,
+          instances: selectedInstances,
+          startDate: startDateValue ? moment(startDateValue).valueOf() * 1000000 : undefined,
+          endDate: endDateValue ? moment(endDateValue).valueOf() * 1000000 : undefined,
+          limit: selectedLimit,
+        })
+      );
+      dispatch(
+        asyncGetLevels({
+          startDate: startDateValue ? moment(startDateValue).valueOf() * 1000000 : undefined,
+        })
+      );
+      dispatch(
+        asyncGetInstances({
+          startDate: startDateValue ? moment(startDateValue).valueOf() * 1000000 : undefined,
+        })
+      );
+    }, 1000),
+    [startDateValue, endDateValue, selectedInstances, selectedLevels, selectedLimit, moduleName]
+  );
 
   useEffect(() => {
-    dispatch(
-      asyncGetAuthenticationQueryRange({
-        query: query,
-        startDate: moment(startDateValue).valueOf() * 1000000,
-        endDate: moment(endDateValue).valueOf() * 1000000,
-      })
-    );
+    requestDebounce();
+    return () => requestDebounce.cancel();
+  }, [
+    dispatch,
+    endDateValue,
+    moduleName,
+    requestDebounce,
+    selectedInstances,
+    selectedLevels,
+    selectedLimit,
+    startDateValue,
+  ]);
 
-    dispatch(
-      asyncGetAuthenticationLevels({
-        query: query,
-        startDate: moment(startDateValue).valueOf() * 1000000,
-      })
-    );
-  }, [dispatch, endDateValue, query, startDateValue]);
-
-  const handleChangeLabels = (event: SelectChangeEvent<typeof selectedLevels>) => {
+  const handleChangeLevels = (event: SelectChangeEvent<unknown>) => {
     const {
       target: { value },
     } = event;
-    setSelectedLevels(value);
+    setSelectedLevels(value as string[]);
   };
 
-  // const handleChangeInstances = (event: SelectChangeEvent<typeof instances>) => {
-  //   const {
-  //     target: { value },
-  //   } = event;
-  //   setInstances(value);
-  // };
+  const handleChangeInstances = (event: SelectChangeEvent<unknown>) => {
+    const {
+      target: { value },
+    } = event;
+    setInstances(value as string[]);
+  };
 
-  const handleStartDateChange = (newValue: MomentInput | null) => {
+  const handleStartDateChange = (newValue: Moment | null) => {
     setStartDateValue(newValue);
   };
-  const handleEndDateChange = (newValue: MomentInput | null) => {
+  const handleEndDateChange = (newValue: Moment | null) => {
     setEndDateValue(newValue);
   };
 
-  // const instancesData = useMemo(() => {
-  //   const instancesArr: string[] = [];
-  //   values?.forEach((item) =>
-  //     item?.values?.forEach((value) => {
-  //       instancesArr.push(item?.stream?.instance);
-  //     })
-  //   );
-  //
-  //   const final = instancesArr;
-  //   return final;
-  // }, [values]);
+  const handleChangeLimit = (newValue: SelectChangeEvent<unknown>) => {
+    setSelectedLimit(newValue?.target?.value as number);
+  };
 
   const prepareData = useMemo(() => {
     const arr: Array<Array<string>> = [];
-    // const instancesArr: string[] = [];
     values?.forEach((item) =>
       item?.values?.forEach((value) => {
         arr.push([...value, item?.stream?.level, item?.stream?.instance]);
-        // instancesArr.push(item.stream.instance);
       })
     );
     return arr;
   }, [values]);
 
+  const refreshRequest = useCallback(
+    throttle(
+      () =>
+        dispatch(
+          asyncGetQueryRange({
+            module: moduleName,
+            levels: selectedLevels,
+            instances: selectedInstances,
+            startDate: startDateValue ? moment(startDateValue).valueOf() * 1000000 : undefined,
+            endDate: endDateValue ? moment(endDateValue).valueOf() * 1000000 : undefined,
+            limit: selectedLimit,
+          })
+        ),
+      1000
+    ),
+    [selectedInstances, selectedLimit, selectedLevels, moduleName, endDateValue, startDateValue]
+  );
+
   const handleRefresh = useCallback(() => {
-    dispatch(
-      asyncGetAuthenticationQueryRange({
-        query: query,
-        startDate: moment(startDateValue).valueOf() * 1000000,
-        endDate: moment().valueOf() * 1000000,
-      })
-    );
-  }, [dispatch, query, startDateValue]);
+    refreshRequest();
+  }, [refreshRequest]);
 
-  const handleGetMoreItems = useCallback(() => {
-    dispatch(
-      asyncGetAuthenticationQueryRange({
-        query: query,
-        startDate: moment(startDateValue).valueOf() * 1000000,
-        endDate: moment().valueOf() * 1000000,
-      })
-    );
-  }, [dispatch, query, startDateValue]);
+  const handleClearStartDateTime = () => {
+    setStartDateValue(null);
+  };
 
-  const debouncedGetApiItems = debounce(() => handleGetMoreItems(), 300);
-
-  const loadMoreItems = async () => {
-    debouncedGetApiItems();
+  const handleClearEndDateTime = () => {
+    setEndDateValue(null);
   };
 
   return (
-    <Paper sx={{ p: 4, borderRadius: 8 }}>
+    <Paper
+      sx={{
+        p: 4,
+        borderRadius: 8,
+        justifyContent: 'space-between',
+      }}>
       <Toolbar
         disableGutters
         sx={{
           display: 'flex',
           justifyContent: 'space-between',
           flexWrap: 'wrap',
+          alignItems: 'stretch',
         }}>
         <Typography sx={{ fontSize: 24, mt: 2 }}>Logs</Typography>
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
-          <IconButton aria-label="refresh" sx={{ mr: 1, mt: 2 }} onClick={() => handleRefresh()}>
-            <RefreshIcon />
-          </IconButton>
-          {/*<FormControl sx={{ m: 1, width: 150 }}>*/}
-          {/*  <InputLabel id="demo-multiple-checkbox-label">Instance</InputLabel>*/}
-          {/*  <Select*/}
-          {/*    sx={{ borderRadius: 3 }}*/}
-          {/*    name={'Instance'}*/}
-          {/*    label={'Instance'}*/}
-          {/*    value={selectedInstances}*/}
-          {/*    multiple*/}
-          {/*    disabled={instancesData?.length === 0}*/}
-          {/*    onChange={handleChangeInstances}*/}
-          {/*    renderValue={(selected) => selected.join(', ')}>*/}
-          {/*    {instancesData.map((item, index) => (*/}
-          {/*      <MenuItem key={index} value={item}>*/}
-          {/*        <Checkbox checked={selectedInstances.indexOf(item) > -1} />*/}
-          {/*        <ListItemText primary={item} />*/}
-          {/*      </MenuItem>*/}
-          {/*    ))}*/}
-          {/*  </Select>*/}
-          {/*</FormControl>*/}
-          <FormControl sx={{ mr: 1, width: 120, mt: 2 }}>
-            <InputLabel id="demo-multiple-checkbox-label">Level</InputLabel>
-            <Select
-              sx={{ borderRadius: 3 }}
-              name={'Level'}
-              label={'Level'}
-              value={selectedLevels}
-              multiple
-              disabled={logsLevels?.length === 0}
-              onChange={handleChangeLabels}
-              renderValue={(selected) => selected.join(', ')}>
-              {logsLevels?.map((item, index) => (
-                <MenuItem key={index} value={item}>
-                  <Checkbox checked={selectedLevels?.indexOf(item) > -1} />
-                  <ListItemText primary={item} />
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <Box sx={{ display: 'flex', alignItems: 'center', minWidth: 450, mt: 2 }}>
+        <Box display={'flex'} flexDirection={'column'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', minWidth: 330, mt: 2 }}>
+            <IconButton aria-label="refresh" sx={{ mr: 1 }} onClick={() => handleRefresh()}>
+              <RefreshIcon />
+            </IconButton>
             <LocalizationProvider dateAdapter={AdapterMoment}>
               <DateTimePicker
                 disableFuture={true}
-                renderInput={(props) => <TextField {...props} />}
-                label="Select Date"
+                maxDateTime={moment(endDateValue).subtract(1, 'minutes')}
+                DialogProps={{
+                  disableRestoreFocus: true,
+                }}
+                onClose={() => setIsStartDatePickerOpen(false)}
+                onOpen={() => setIsStartDatePickerOpen(true)}
+                open={isStartDatePickerOpen}
+                renderInput={(props) => (
+                  <TextField
+                    {...props}
+                    InputLabelProps={{ ...props.InputLabelProps, shrink: true }}
+                    label={'Start Date'}
+                    sx={{ ...props.sx, width: 302 }}
+                    inputProps={{ ...props.inputProps, placeholder: 'one hour ago' }}
+                    onClick={() => setIsStartDatePickerOpen(true)}
+                  />
+                )}
                 value={startDateValue}
                 onAccept={(newValue) => {
                   handleStartDateChange(newValue);
                 }}
                 onChange={() => {}}
+                InputProps={{
+                  sx: { mr: 1 },
+                  startAdornment: startDateValue && (
+                    <IconButton
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleClearStartDateTime();
+                      }}
+                      sx={{ padding: 0, marginRight: 1, cursor: 'pointer' }}>
+                      <CloseIcon color={'inherit'} />
+                    </IconButton>
+                  ),
+                }}
               />
             </LocalizationProvider>
-            <Typography sx={{ marginX: 1 }}>to</Typography>
             <LocalizationProvider dateAdapter={AdapterMoment}>
               <DateTimePicker
-                maxDateTime={moment()}
-                minDateTime={startDateValue}
                 disableFuture={true}
-                renderInput={(props) => <TextField {...props} />}
-                label="Select Date"
+                minDateTime={startDateValue ? startDateValue : moment().subtract(1, 'hours')}
+                DialogProps={{
+                  disableRestoreFocus: true,
+                }}
+                onClose={() => setIsEndDatePickerOpen(false)}
+                onOpen={() => setIsEndDatePickerOpen(true)}
+                open={isEndDatePickerOpen}
+                renderInput={(props) => (
+                  <TextField
+                    {...props}
+                    InputLabelProps={{ ...props.InputLabelProps, shrink: true }}
+                    label={'End Date'}
+                    sx={{ ...props.sx, width: 302 }}
+                    inputProps={{ ...props.inputProps, placeholder: 'now' }}
+                    onClick={() => setIsEndDatePickerOpen(true)}
+                  />
+                )}
                 value={endDateValue}
                 onAccept={(newValue) => {
                   handleEndDateChange(newValue);
                 }}
                 onChange={() => {}}
+                InputProps={{
+                  startAdornment: endDateValue && (
+                    <IconButton
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleClearEndDateTime();
+                      }}
+                      sx={{ padding: 0, marginRight: 1, cursor: 'pointer' }}>
+                      <CloseIcon color={'inherit'} />
+                    </IconButton>
+                  ),
+                }}
               />
             </LocalizationProvider>
+            <FormControl sx={{ ml: 1, minWidth: 90 }}>
+              <InputLabel id="demo-multiple-checkbox-label">Limit</InputLabel>
+              <Select
+                sx={{ borderRadius: 3 }}
+                name={'limit'}
+                label={'Limit'}
+                value={selectedLimit}
+                disabled={Limits?.length === 0}
+                onChange={handleChangeLimit}
+                renderValue={(selected) => selected.toString()}>
+                {Limits.map((item, index) => (
+                  <MenuItem key={index} value={item}>
+                    <Checkbox checked={selectedLimit === item} />
+                    <ListItemText primary={item} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+          <Box display={'flex'}>
+            <FormControl sx={{ mt: 2, mr: 1, width: '100%' }}>
+              <InputLabel id="demo-multiple-checkbox-label">Instance</InputLabel>
+              <Select
+                sx={{ borderRadius: 3 }}
+                fullWidth
+                name={'Instance'}
+                label={'Instance'}
+                value={selectedInstances}
+                multiple
+                disabled={instances?.length === 0}
+                onChange={handleChangeInstances}
+                renderValue={(selected) => selected.join(', ')}>
+                {instances?.map((item, index) => (
+                  <MenuItem key={index} value={item}>
+                    <Checkbox checked={selectedInstances.indexOf(item) > -1} />
+                    <ListItemText primary={item} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl sx={{ mt: 2, width: '100%' }}>
+              <InputLabel id="demo-multiple-checkbox-label">Level</InputLabel>
+              <Select
+                sx={{ borderRadius: 3 }}
+                fullWidth
+                name={'Level'}
+                label={'Level'}
+                value={selectedLevels}
+                multiple
+                disabled={logsLevels?.length === 0}
+                onChange={handleChangeLevels}
+                renderValue={(selected) => selected.join(', ')}>
+                {logsLevels?.map((item, index) => (
+                  <MenuItem key={index} value={item}>
+                    <Checkbox checked={selectedLevels?.indexOf(item) > -1} />
+                    <ListItemText primary={item} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
           </Box>
         </Box>
       </Toolbar>
       <Box
         sx={{
-          height: '65vh',
+          height: '55vh',
           background: 'background.paper',
-          display: 'flex',
-          flexGrow: 1,
-          mt: 1,
+          mt: 2,
         }}>
         {prepareData?.length ? (
-          <LogsList data={prepareData} loadMoreItems={loadMoreItems} />
+          <LogsList data={prepareData} />
         ) : (
           <Typography
             sx={{ display: 'flex', flex: 1, alignSelf: 'center', justifyContent: 'center' }}>
