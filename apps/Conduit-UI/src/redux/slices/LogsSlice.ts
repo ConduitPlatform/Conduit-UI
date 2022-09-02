781b4e6769e0d24d6bf5f0d312c86c1ce041f8d3
Voudromnianos Apostolos <69977309+AVoudromnianos@users.noreply.github.com>
2022-09-02 18:39:57 +0300

    fix: remove instance from logs (#124)

--- apps/Conduit-UI/src/redux/slices/LogsSlice.ts
+++ apps/Conduit-UI/src/redux/slices/LogsSlice.ts
@@ -3,22 +3,16 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { setAppLoading } from './appSlice';
 import { enqueueErrorNotification } from '../../utils/useNotifier';
 import { getErrorData } from '../../utils/error-handler';
-import {
-  getLogsInstances,
-  getLogsLevels,
-  getLogsQueryRange,
-} from '../../http/requests/LogsRequests';
+import { getLogsLevels, getLogsQueryRange } from '../../http/requests/LogsRequests';
 
 interface ILogsSlice {
   logs: Record<ModulesTypes, LogsData[]>;
   levels: string[];
-  instances: string[];
   modules: ModulesTypes[];
 }
 
 const initialState: ILogsSlice = {
   logs: {} as Record<ModulesTypes, LogsData[]>,
-  instances: [],
   modules: [],
   levels: [],
 };
@@ -42,31 +36,11 @@ export const asyncGetLevels = createAsyncThunk(
   }
 );
 
-export const asyncGetInstances = createAsyncThunk(
-  '/authentication/getInstances',
-  async (body: { startDate?: number; endDate?: number }, thunkAPI) => {
-    thunkAPI.dispatch(setAppLoading(true));
-    try {
-      const {
-        data: { data },
-      } = await getLogsInstances(body);
-
-      thunkAPI.dispatch(setAppLoading(false));
-      return data;
-    } catch (error) {
-      thunkAPI.dispatch(setAppLoading(false));
-      thunkAPI.dispatch(enqueueErrorNotification(`${getErrorData(error)}`));
-      throw error;
-    }
-  }
-);
-
 export const asyncGetQueryRange = createAsyncThunk(
   '/authentication/getQueryRange',
   async (
     body: {
       module: ModulesTypes;
-      instances?: string[];
       levels?: string[];
       startDate?: number;
       endDate?: number;
@@ -117,9 +91,6 @@ const logsSlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(asyncGetLevels.fulfilled, (state, action) => {
       state.levels = action.payload;
-    });
-    builder.addCase(asyncGetInstances.fulfilled, (state, action) => {
-      state.instances = action.payload;
     });
     builder.addCase(asyncGetQueryRange.fulfilled, (state, action) => {
       state.logs[action.meta.arg.module] = action.payload;
