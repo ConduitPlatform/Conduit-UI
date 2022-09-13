3530d8a8901d9f92c53bf4d6cdaf568b6b04924b
Voudromnianos Apostolos <69977309+AVoudromnianos@users.noreply.github.com>
2022-09-13 15:45:18 +0300

    fix: phone number regular expression (#133)

--- apps/Conduit-UI/src/utils/validations.ts
+++ apps/Conduit-UI/src/utils/validations.ts
@@ -5,6 +5,6 @@ export const usernameRegExp = /^[a-zA-Z0-9]([_-](?![_-])|[a-zA-Z0-9]){3,14}[a-zA
 export const emailRegExp =
   /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
 
-export const phoneNumberRegExp = /^[0-9]{4,12}$/;
+export const phoneNumberRegExp = /^([+]?)[0-9]{4,12}$/;
 
 export const noSpacesOrSpecialChars = /^(\d|\w)+$/;
