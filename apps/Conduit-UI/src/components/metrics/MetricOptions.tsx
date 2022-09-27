58065be527848525f28ec8af8d8fa5c4d4b2e03c
Kostas <classcreator21@outlook.com>
2022-09-27 16:34:58 +0300

    feat(MetricOptions): added simple view

--- apps/Conduit-UI/src/components/metrics/MetricOptions.tsx
+++ apps/Conduit-UI/src/components/metrics/MetricOptions.tsx
@@ -1,9 +1,10 @@
-import { Close } from '@mui/icons-material';
+import { Close, CloseOutlined, InfoOutlined, OpenInFull } from '@mui/icons-material';
 import { DateTimePicker, LocalizationProvider } from '@mui/lab';
 import AdapterMoment from '@mui/lab/AdapterMoment';
 import {
   Box,
   FormControl,
+  Icon,
   IconButton,
   InputLabel,
   ListItemText,
@@ -11,6 +12,7 @@ import {
   Select,
   SelectChangeEvent,
   TextField,
+  Tooltip,
 } from '@mui/material';
 import moment, { Moment } from 'moment';
 import React, { FC, useMemo, useState } from 'react';
@@ -25,6 +27,8 @@ interface Props {
   steps: string[];
 }
 
+const simpleViewOptions = ['Last hour', 'Last 12h', 'Last 24h', 'Last week'];
+
 const MetricOptions: FC<Props> = ({
   startDateValue,
   setStartDateValue,
@@ -36,6 +40,8 @@ const MetricOptions: FC<Props> = ({
 }) => {
   const [isStartDatePickerOpen, setIsStartDatePickerOpen] = useState<boolean>(false);
   const [isEndDatePickerOpen, setIsEndDatePickerOpen] = useState<boolean>(false);
+  const [detailedView, setDetailedView] = useState<boolean>(false);
+  const [simpleViewValue, setSimpleViewValue] = useState<string>('Last hour');
 
   const minDateOfStart = useMemo(() => {
     return endDateValue ? moment(endDateValue).subtract(1, 'years') : moment().subtract(1, 'years');
@@ -76,6 +82,25 @@ const MetricOptions: FC<Props> = ({
     setSelectedStep(value as string);
   };
 
+  const handleChangeSimpleViewStep = (event: SelectChangeEvent<string>) => {
+    const {
+      target: { value },
+    } = event;
+
+    setSimpleViewValue(value);
+
+    if (value === 'Last hour') {
+      setSelectedStep('10m');
+      setStartDateValue(moment().subtract(1, 'hours'));
+    } else if (value === 'Last 12h') {
+      setSelectedStep('10m');
+      setStartDateValue(moment().subtract(12, 'hours'));
+    } else if (value === 'Last 24h') {
+      setSelectedStep('10m');
+      setStartDateValue(moment().subtract(24, 'hours'));
+    }
+  };
+
   const minDateOfEnd = useMemo(() => {
     return startDateValue
       ? moment(startDateValue).add(1, 'minutes')
@@ -86,112 +111,165 @@ const MetricOptions: FC<Props> = ({
     return undefined;
   }, []);
 
+  const handleChangeToDetailed = () => {
+    setDetailedView(true);
+    setStartDateValue(null);
+    setEndDateValue(null);
+    setSelectedStep('10m');
+  };
+
+  const handleChangeToSimple = () => {
+    setDetailedView(false);
+    setStartDateValue(moment().subtract(1, 'hours'));
+    setEndDateValue(moment());
+    setSelectedStep('10m');
+  };
+
   return (
-    <Box display="flex" py={1} gap={2}>
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-        <DateTimePicker
-          disableFuture={true}
-          minDateTime={minDateOfStart}
-          maxDateTime={maxDateOfStart}
-          DialogProps={{
-            disableRestoreFocus: true,
-          }}
-          onClose={() => setIsStartDatePickerOpen(false)}
-          onOpen={() => setIsStartDatePickerOpen(true)}
-          open={isStartDatePickerOpen}
-          renderInput={(props) => (
-            <TextField
-              {...props}
-              fullWidth
-              size={'small'}
-              InputLabelProps={{ ...props.InputLabelProps, shrink: true }}
-              label={'Start Date'}
-              inputProps={{ ...props.inputProps, placeholder: 'one hour ago' }}
-              onClick={() => setIsStartDatePickerOpen(true)}
+    <>
+      {detailedView ? (
+        <Box display="flex" p={1} gap={2}>
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <DateTimePicker
+              disableFuture={true}
+              minDateTime={minDateOfStart}
+              maxDateTime={maxDateOfStart}
+              DialogProps={{
+                disableRestoreFocus: true,
+              }}
+              onClose={() => setIsStartDatePickerOpen(false)}
+              onOpen={() => setIsStartDatePickerOpen(true)}
+              open={isStartDatePickerOpen}
+              renderInput={(props) => (
+                <TextField
+                  {...props}
+                  fullWidth
+                  size={'small'}
+                  InputLabelProps={{ ...props.InputLabelProps, shrink: true }}
+                  label={'Start Date'}
+                  inputProps={{ ...props.inputProps, placeholder: 'one hour ago' }}
+                  onClick={() => setIsStartDatePickerOpen(true)}
+                />
+              )}
+              value={startDateValue}
+              onAccept={(newValue) => {
+                handleStartDateChange(newValue);
+              }}
+              onChange={() => {
+                return;
+              }}
+              InputProps={{
+                startAdornment: startDateValue && (
+                  <IconButton
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleClearStartDateTime();
+                    }}
+                    sx={{ padding: 0, marginRight: 1, cursor: 'pointer' }}>
+                    <Close color={'inherit'} />
+                  </IconButton>
+                ),
+              }}
             />
-          )}
-          value={startDateValue}
-          onAccept={(newValue) => {
-            handleStartDateChange(newValue);
-          }}
-          onChange={() => {
-            return;
-          }}
-          InputProps={{
-            startAdornment: startDateValue && (
-              <IconButton
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleClearStartDateTime();
-                }}
-                sx={{ padding: 0, marginRight: 1, cursor: 'pointer' }}>
-                <Close color={'inherit'} />
-              </IconButton>
-            ),
-          }}
-        />
-      </LocalizationProvider>
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-        <DateTimePicker
-          disableFuture={true}
-          maxDateTime={maxDateOfEnd}
-          minDateTime={minDateOfEnd}
-          DialogProps={{
-            disableRestoreFocus: true,
-          }}
-          onClose={() => setIsEndDatePickerOpen(false)}
-          onOpen={() => setIsEndDatePickerOpen(true)}
-          open={isEndDatePickerOpen}
-          renderInput={(props) => (
-            <TextField
-              {...props}
-              fullWidth
-              size={'small'}
-              InputLabelProps={{ ...props.InputLabelProps, shrink: true }}
-              label={'End Date'}
-              inputProps={{ ...props.inputProps, placeholder: 'now' }}
-              onClick={() => setIsEndDatePickerOpen(true)}
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <DateTimePicker
+              disableFuture={true}
+              maxDateTime={maxDateOfEnd}
+              minDateTime={minDateOfEnd}
+              DialogProps={{
+                disableRestoreFocus: true,
+              }}
+              onClose={() => setIsEndDatePickerOpen(false)}
+              onOpen={() => setIsEndDatePickerOpen(true)}
+              open={isEndDatePickerOpen}
+              renderInput={(props) => (
+                <TextField
+                  {...props}
+                  fullWidth
+                  size={'small'}
+                  InputLabelProps={{ ...props.InputLabelProps, shrink: true }}
+                  label={'End Date'}
+                  inputProps={{ ...props.inputProps, placeholder: 'now' }}
+                  onClick={() => setIsEndDatePickerOpen(true)}
+                />
+              )}
+              value={endDateValue}
+              onAccept={(newValue) => {
+                handleEndDateChange(newValue);
+              }}
+              onChange={() => {
+                return;
+              }}
+              InputProps={{
+                startAdornment: endDateValue && (
+                  <IconButton
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleClearEndDateTime();
+                    }}
+                    sx={{ padding: 0, marginRight: 1, cursor: 'pointer' }}>
+                    <Close color={'inherit'} />
+                  </IconButton>
+                ),
+              }}
             />
-          )}
-          value={endDateValue}
-          onAccept={(newValue) => {
-            handleEndDateChange(newValue);
-          }}
-          onChange={() => {
-            return;
-          }}
-          InputProps={{
-            startAdornment: endDateValue && (
-              <IconButton
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleClearEndDateTime();
-                }}
-                sx={{ padding: 0, marginRight: 1, cursor: 'pointer' }}>
-                <Close color={'inherit'} />
-              </IconButton>
-            ),
-          }}
-        />
-      </LocalizationProvider>
-      <FormControl size={'small'} sx={{ minWidth: 88 }}>
-        <InputLabel>Step</InputLabel>
-        <Select
-          sx={{ borderRadius: 3 }}
-          fullWidth
-          name={'Step'}
-          label={'Step'}
-          value={selectedStep}
-          onChange={handleChangeStep}
-          renderValue={(selected) => selected.toString()}>
-          {steps?.map((item, index) => (
-            <MenuItem key={index} value={item}>
-              <ListItemText primary={item} />
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Box>
+          </LocalizationProvider>
+          <FormControl size={'small'} sx={{ minWidth: 88 }}>
+            <InputLabel>Step</InputLabel>
+            <Select
+              sx={{ borderRadius: 3 }}
+              fullWidth
+              name={'Step'}
+              label={'Step'}
+              value={selectedStep}
+              onChange={handleChangeStep}
+              renderValue={(selected) => selected.toString()}>
+              {steps?.map((item, index) => (
+                <MenuItem key={index} value={item}>
+                  <ListItemText primary={item} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Tooltip title="Return to simple view">
+            <IconButton color="primary" onClick={() => handleChangeToSimple()}>
+              <CloseOutlined />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      ) : (
+        <Box display="flex" alignItems="center" justifyContent="flex-end" p={1} gap={2}>
+          <FormControl size={'small'} sx={{ minWidth: 150 }}>
+            <InputLabel>Duration</InputLabel>
+            <Select
+              sx={{ borderRadius: 3 }}
+              fullWidth
+              name={'Duration'}
+              label={'Duration'}
+              value={simpleViewValue}
+              onChange={handleChangeSimpleViewStep}
+              renderValue={(selected) => selected.toString()}>
+              {simpleViewOptions?.map((item, index) => (
+                <MenuItem key={index} value={item}>
+                  <ListItemText primary={item} />
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Tooltip title="The step on the simple view defaults to 10m">
+            <Icon>
+              <InfoOutlined />
+            </Icon>
+          </Tooltip>
+          <Tooltip title="Expanded view">
+            <IconButton color="primary" onClick={() => handleChangeToDetailed()}>
+              <OpenInFull />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      )}
+    </>
   );
 };
 
