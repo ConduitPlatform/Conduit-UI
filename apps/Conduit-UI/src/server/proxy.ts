df3a25ad8477bd6986ba620b6b26790afa5f9cf1
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2022-09-27 20:58:36 +0300

    refactor: proxy to be included only when var is available

--- apps/Conduit-UI/src/server/proxy.ts
+++ apps/Conduit-UI/src/server/proxy.ts
@@ -10,22 +10,33 @@ export const proxy = httpProxy.createProxyServer({
   autoRewrite: false,
 });
 
-export const proxyLoki = httpProxy.createProxyServer({
-  /**
-   * Get the actual back-end service url from env variables.
-   * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
-   */
-  target: process.env.LOKI_URL || 'NULL',
-  changeOrigin: true,
-  autoRewrite: false,
-});
-
-export const proxyPrometheus = httpProxy.createProxyServer({
-  /**
-   * Get the actual back-end service url from env variables.
-   * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
-   */
-  target: process.env.PROMETHEUS_URL || 'NULL',
-  changeOrigin: true,
-  autoRewrite: false,
-});
+export const proxyLoki = (() => {
+  if (process.env.LOKI_URL && process.env.LOKI_URL.length > 0) {
+    return httpProxy.createProxyServer({
+      /**
+       * Get the actual back-end service url from env variables.
+       * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
+       */
+      target: process.env.LOKI_URL || 'NULL',
+      changeOrigin: true,
+      autoRewrite: false,
+    });
+  } else {
+    return null;
+  }
+})();
+export const proxyPrometheus = (() => {
+  if (process.env.PROMETHEUS_URL && process.env.PROMETHEUS_URL.length > 0) {
+    return httpProxy.createProxyServer({
+      /**
+       * Get the actual back-end service url from env variables.
+       * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
+       */
+      target: process.env.PROMETHEUS_URL,
+      changeOrigin: true,
+      autoRewrite: false,
+    });
+  } else {
+    return null;
+  }
+})();
