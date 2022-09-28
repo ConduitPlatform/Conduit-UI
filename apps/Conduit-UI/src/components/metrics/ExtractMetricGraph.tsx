361444e4db93b501e2b0762f64624349e2a8cb7e
Kostas <classcreator21@outlook.com>
2022-09-28 12:52:04 +0300

    fix(ExtractMetricGraph): remove log

--- apps/Conduit-UI/src/components/metrics/ExtractMetricGraph.tsx
+++ apps/Conduit-UI/src/components/metrics/ExtractMetricGraph.tsx
@@ -35,8 +35,6 @@ const ExtractQueryRangeGraph: FC<Props> = ({
     (state) => state?.metricsSlice?.meta.genericMetricLoading?.[expression]
   );
 
-  console.log(data);
-
   useEffect(() => {
     dispatch(
       asyncGetGenericMetricQueryRange({
