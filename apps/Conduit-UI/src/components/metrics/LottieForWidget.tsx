bd30d832dc86fe3ef7eb88657f74e28e00cd2c3e
Kostas <classcreator21@outlook.com>
2022-09-30 12:12:07 +0300

    fix(Lotties): widget lotties sizing

--- apps/Conduit-UI/src/components/metrics/LottieForWidget.tsx
+++ apps/Conduit-UI/src/components/metrics/LottieForWidget.tsx
@@ -31,18 +31,18 @@ const LottieForWidget: FC<Props> = ({ small, lottieFile }) => {
       } else if (md) {
         return 19;
       } else if (lg) {
-        return 26;
+        return 28;
       } else {
-        return 26;
+        return 30;
       }
     } else if (xs || sm) {
       return 24;
     } else if (md) {
       return 26;
     } else if (lg) {
-      return 30;
+      return 32;
     } else {
-      return 35;
+      return 38;
     }
   };
 
