2c222c6d648fbe52fe643f4b1f3c6f5691d689f1
Avoudromnianos <aposbou@gmail.com>
2022-10-11 17:32:37 +0300

    fix: schema object, json view responsiveness

--- apps/Conduit-UI/src/components/database/schemas/SchemaOverview/SchemaViewer.tsx
+++ apps/Conduit-UI/src/components/database/schemas/SchemaOverview/SchemaViewer.tsx
@@ -73,17 +73,8 @@ const SchemaViewer: FC<Props> = ({
   };
 
   return (
-    <Box {...rest}>
-      <Paper
-        elevation={0}
-        sx={{
-          height: '64vh',
-          border: '1px',
-          paddingX: 3,
-          paddingY: 1,
-          borderRadius: '4px',
-          overflow: 'auto',
-        }}>
+    <Box {...rest} sx={{ borderRadius: 4, overflow: 'hidden', height: '100%' }}>
+      <Paper elevation={0} sx={{ overflow: 'auto', height: '100%', padding: 2 }}>
         {data &&
           Array.isArray(data[dataKey]) &&
           data[dataKey].length > 0 &&
@@ -94,7 +85,6 @@ const SchemaViewer: FC<Props> = ({
                 display: 'flex',
                 flexDirection: 'column-reverse',
                 padding: 1,
-                mb: 1,
               }}>
               <Grid container alignItems="center">
                 <Grid item xs={editable ? 3 : 6}>
