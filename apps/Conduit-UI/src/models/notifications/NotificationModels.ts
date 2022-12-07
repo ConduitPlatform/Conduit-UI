870562854b3b0df59c9117f8cc85815508c8a2a3
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2022-12-07 15:40:43 +0200

    feat(push-notifications): oneSignal support

--- apps/Conduit-UI/src/models/notifications/NotificationModels.ts
+++ apps/Conduit-UI/src/models/notifications/NotificationModels.ts
@@ -7,20 +7,28 @@ export interface NotificationData {
 export interface INotificationConfig {
   active: boolean;
   providerName: string;
-  firebase: FirebaseSettings;
+  firebase?: FirebaseSettings;
+  onesignal?: OneSignalSettings;
   message?: string;
 }
 
 export enum INotificationProviders {
   firebase = 'firebase',
+  onesignal = 'onesignal',
 }
 
 export interface INotificationProviderSettings {
   firebase: FirebaseSettings;
+  onesignal: OneSignalSettings;
 }
 
 export interface FirebaseSettings {
   projectId: string;
   privateKey: string;
   clientEmail: string;
+}
+
+export interface OneSignalSettings {
+  appId: string;
+  apiKey: string;
 }
