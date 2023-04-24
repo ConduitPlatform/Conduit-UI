7c02dc193a42a85d77c2d5177fe565d93e5ab270
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2023-04-24 19:51:43 +0300

    feat: get routes from router request feat: add rpc functions feat: add coloring in nodes

--- apps/Conduit-UI/src/features/router/components/routeGrid.tsx
+++ apps/Conduit-UI/src/features/router/components/routeGrid.tsx
@@ -10,170 +10,9 @@ import ReactFlow, {
   Node,
   Position,
 } from 'reactflow';
+import { useAppDispatch, useAppSelector } from '../../../redux/store';
+import { asyncGetAvailableRoutes } from '../store/routerSlice';
 
-const endpoints = {
-  authentication: {
-    moduleUrl: '0.0.0.0:55154',
-    routes: {
-      PostLocalNew: {
-        action: 'POST',
-        path: '/authentication/local/new',
-        description: 'Creates a new user using email/password.',
-        middlewares: [],
-      },
-      PostLocal: {
-        action: 'POST',
-        path: '/authentication/local',
-        description:
-          'Login endpoint that can be used to authenticate.\n         Tokens are returned according to configuration.',
-        middlewares: [],
-      },
-      PostLocalChangePassword: {
-        action: 'POST',
-        path: '/authentication/local/change-password',
-        description:
-          "Changes the user's password but requires the old password first.\n            " +
-          '     If 2FA is enabled then a message will be returned asking for token input.',
-        middlewares: ['authMiddleware'],
-      },
-      PostLocalChangeEmail: {
-        action: 'POST',
-        path: '/authentication/local/change-email',
-        description: "Changes the user's email (requires sudo access).",
-        middlewares: ['authMiddleware'],
-      },
-      GetHookVerifyEmailVerificationtoken: {
-        action: 'GET',
-        path: '/hook/authentication/verify-email/:verificationToken',
-        description:
-          'A webhook used to verify user email. This bypasses the need for client id/secret.',
-        middlewares: [],
-      },
-      GetHookVerifyChangeEmailVerificationtoken: {
-        action: 'GET',
-        path: '/hook/authentication/verify-change-email/:verificationToken',
-        description:
-          'A webhook used to verify an email address change. This bypasses the need for client id/secret.',
-        middlewares: [],
-      },
-      PostService: {
-        action: 'POST',
-        path: '/authentication/service',
-        description: 'Login with service account.',
-        middlewares: [],
-      },
-      GetUser: {
-        action: 'GET',
-        path: '/authentication/user',
-        description: 'Returns the authenticated user.',
-        middlewares: ['authMiddleware'],
-      },
-      DeleteUser: {
-        action: 'DELETE',
-        path: '/authentication/user',
-        description: 'Deletes the authenticated user.',
-        middlewares: ['authMiddleware'],
-      },
-      PostRenew: {
-        action: 'POST',
-        path: '/authentication/renew',
-        description:
-          "Renews the access and refresh tokens. Requires a valid refresh token provided in Authorization header/cookie. Format 'Bearer TOKEN'",
-        middlewares: ['authMiddleware'],
-      },
-      PostLogout: {
-        action: 'POST',
-        path: '/authentication/logout',
-        description: 'Logs out authenticated user.',
-        middlewares: ['authMiddleware'],
-      },
-    },
-    socketRoutes: {},
-    proxyRoutes: {},
-    middlewares: {
-      authMiddleware: {
-        path: '/authentication/',
-      },
-      captchaMiddleware: {
-        path: '/authentication/',
-      },
-    },
-  },
-  chat: {
-    moduleUrl: '0.0.0.0:55156',
-    routes: {
-      PostRooms: {
-        action: 'POST',
-        path: '/chat/rooms',
-        description: 'Creates a new room.',
-        middlewares: ['authMiddleware'],
-      },
-      PutRoomsRoomidAddusers: {
-        action: 'PUT',
-        path: '/chat/rooms/:roomId/addUsers',
-        description: 'Adds users to a chat room.',
-        middlewares: ['authMiddleware'],
-      },
-      PutLeaveRoomid: {
-        action: 'PUT',
-        path: '/chat/leave/:roomId',
-        description: 'Removes current user from a chat room.',
-        middlewares: ['authMiddleware'],
-      },
-      GetRoomsId: {
-        action: 'GET',
-        path: '/chat/rooms/:id',
-        description: 'Returns a chat room.',
-        middlewares: ['authMiddleware'],
-      },
-      GetRooms: {
-        action: 'GET',
-        path: '/chat/rooms',
-        description: 'Returns queried chat rooms.',
-        middlewares: ['authMiddleware'],
-      },
-      GetMessagesId: {
-        action: 'GET',
-        path: '/chat/messages/:id',
-        description: 'Returns a message.',
-        middlewares: ['authMiddleware'],
-      },
-      GetMessages: {
-        action: 'GET',
-        path: '/chat/messages',
-        description: 'Returns queried messages and their total count.',
-        middlewares: ['authMiddleware'],
-      },
-      DeleteMessagesMessageid: {
-        action: 'DELETE',
-        path: '/chat/messages/:messageId',
-        description: 'Deletes a message.',
-        middlewares: ['authMiddleware'],
-      },
-      PutMessagesMessageid: {
-        action: 'PUT',
-        path: '/chat/messages/:messageId',
-        description: 'Updates content of a message.',
-        middlewares: ['authMiddleware'],
-      },
-    },
-    socketRoutes: {
-      '/chat/': {
-        connect: {
-          middlewares: ['authMiddleware'],
-        },
-        message: {
-          middlewares: ['authMiddleware'],
-        },
-        messagesRead: {
-          middlewares: ['authMiddleware'],
-        },
-      },
-    },
-    proxyRoutes: {},
-    middlewares: {},
-  },
-};
 type NodeType = 'ModulePath' | 'Middleware' | 'Route' | 'URL';
 
 const nodeWidth = 250;
@@ -187,6 +26,18 @@ const createNode = (
   id,
   type,
   position,
+  style: {
+    borderColor:
+      type === 'Middleware'
+        ? '#0000ff'
+        : type === 'Route'
+        ? '#00ff00'
+        : type === 'URL'
+        ? '#ff0000'
+        : type === 'ModulePath'
+        ? '#000000'
+        : '#000000',
+  },
   sourcePosition: Position.Right,
   targetPosition: Position.Left,
   data: { label: label || `${id}` },
@@ -209,7 +60,12 @@ const generateElements = (data: any) => {
     const moduleUrl = data[moduleName].moduleUrl;
     const modulePath = data[moduleName].middlewares[0];
 
-    nodes.push(createNode('ModulePath', `/${moduleName}`, { x: xPos, y: yPos }, modulePath));
+    const middleYPos = yPos + 100 + Object.keys(data[moduleName].routes).length * 100;
+    nodes.push(
+      createNode('ModulePath', `/${moduleName}`, { x: xPos - 100, y: middleYPos }, modulePath)
+    );
+
+    let maxXPos = xPos;
 
     for (const routeName in data[moduleName].routes) {
       const route = data[moduleName].routes[routeName];
@@ -218,11 +74,18 @@ const generateElements = (data: any) => {
       const routeId = `${moduleName}-${routeName}`;
 
       yPos += 100;
+      nodes.push(createNode('Route', routeId, { x: xPos + nodeWidth, y: yPos }, routePath));
+      edges.push(createEdge(`/${moduleName}`, routeId));
 
-      let lastXPos = xPos;
+      edges.push(createEdge(route.handler, `${moduleName}-url`));
+
+      let lastXPos = xPos + nodeWidth;
       if (routeMiddlewares.length > 0) {
         routeMiddlewares.forEach((middleware: string, index: number) => {
           lastXPos = lastXPos + 2 * index + nodeWidth;
+          if (lastXPos > maxXPos) {
+            maxXPos = lastXPos;
+          }
           const middlewareId = `${routeId}-middleware-${index}`;
           const previousMiddlewareId = `${routeId}-middleware-${index - 1}`;
           nodes.push(
@@ -237,33 +100,42 @@ const generateElements = (data: any) => {
             )
           );
           if (index === 0 && routeMiddlewares.length === 1) {
-            edges.push(createEdge(`/${moduleName}`, middlewareId));
-            edges.push(createEdge(middlewareId, `${routeId}`));
+            edges.push(createEdge(routeId, middlewareId));
+            edges.push(createEdge(middlewareId, route.handler));
           } else if (index === 0) {
-            edges.push(createEdge(`/${moduleName}`, middlewareId));
+            edges.push(createEdge(routeId, middlewareId));
           } else if (index === routeMiddlewares.length - 1) {
             edges.push(createEdge(previousMiddlewareId, middlewareId));
-            edges.push(createEdge(middlewareId, `${routeId}`));
+            edges.push(createEdge(middlewareId, route.handler));
           } else {
             edges.push(createEdge(previousMiddlewareId, middlewareId));
           }
         });
       } else {
-        edges.push(createEdge(`/${moduleName}`, routeId));
+        edges.push(createEdge(`${routeId}`, route.handler));
       }
-      nodes.push(createNode('Route', routeId, { x: lastXPos + nodeWidth, y: yPos }, routePath));
       nodes.push(
         createNode(
           'URL',
-          `${routeId}-url`,
-          { x: lastXPos + 3 * nodeWidth, y: yPos },
-          `${moduleName}(${moduleUrl})`
+          route.handler,
+          { x: lastXPos + nodeWidth, y: yPos },
+          `${route.handler} (grpc)`
         )
       );
-      edges.push(createEdge(`${routeId}`, `${routeId}-url`));
-
+      if (lastXPos + 3 * nodeWidth > maxXPos) {
+        maxXPos = lastXPos + 3 * nodeWidth;
+      }
       yPos += 100;
     }
+
+    nodes.push(
+      createNode(
+        'URL',
+        `${moduleName}-url`,
+        { x: maxXPos + 3 * nodeWidth, y: middleYPos },
+        `${moduleName} (${moduleUrl})`
+      )
+    );
   }
 
   return { nodes, edges };
@@ -274,6 +146,12 @@ const onLoad = (reactFlowInstance: any): void => {
 };
 
 const EndpointFlow: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { availableRoutes } = useAppSelector((state) => state.routerSlice.data);
+  useEffect(() => {
+    dispatch(asyncGetAvailableRoutes());
+  }, []);
+
   const [nodes, setNodes] = useState<Node[]>([]);
   const [edges, setEdges] = useState<Edge[]>([]);
 
@@ -286,10 +164,10 @@ const EndpointFlow: React.FC = () => {
     []
   );
   useEffect(() => {
-    const { nodes, edges } = generateElements(endpoints);
+    const { nodes, edges } = generateElements(availableRoutes);
     setNodes(nodes);
     setEdges(edges);
-  }, []);
+  }, [availableRoutes]);
   return (
     <div style={{ height: '80vh', width: '100%' }}>
       <ReactFlow
@@ -303,7 +181,7 @@ const EndpointFlow: React.FC = () => {
         <Background color="#888" gap={16} />
 
         <Controls />
-        <MiniMap />
+        <MiniMap zoomable pannable />
       </ReactFlow>
     </div>
   );
