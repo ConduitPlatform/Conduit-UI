6d8a157436aeaa060e7c8caec56445fd85138363
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2023-12-20 16:00:44 +0200

    refactor: try/catch handlers for proxy

--- apps/Conduit-UI/src/server/proxy.ts
+++ apps/Conduit-UI/src/server/proxy.ts
@@ -9,34 +9,24 @@ export const proxy = httpProxy.createProxyServer({
   changeOrigin: true,
   autoRewrite: false,
 });
+export let proxyLoki: httpProxy | null = null;
 
-export const proxyLoki = (() => {
-  if (process.env.LOKI_URL && process.env.LOKI_URL.length > 0) {
-    return httpProxy.createProxyServer({
-      /**
-       * Get the actual back-end service url from env variables.
-       * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
-       */
-      target: process.env.LOKI_URL || 'NULL',
-      changeOrigin: true,
-      autoRewrite: false,
-    });
-  } else {
-    return null;
-  }
-})();
-export const proxyPrometheus = (() => {
-  if (process.env.PROMETHEUS_URL && process.env.PROMETHEUS_URL.length > 0) {
-    return httpProxy.createProxyServer({
-      /**
-       * Get the actual back-end service url from env variables.
-       * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
-       */
-      target: process.env.PROMETHEUS_URL,
-      changeOrigin: true,
-      autoRewrite: false,
-    });
-  } else {
-    return null;
-  }
-})();
+if (process.env.LOKI_URL && process.env.LOKI_URL.length > 0) {
+  proxyLoki = httpProxy.createProxyServer({
+    target: process.env.LOKI_URL,
+    changeOrigin: true,
+    autoRewrite: false,
+  });
+}
+export let proxyPrometheus: httpProxy | null = null;
+if (process.env.PROMETHEUS_URL && process.env.PROMETHEUS_URL.length > 0) {
+  proxyPrometheus = httpProxy.createProxyServer({
+    /**
+     * Get the actual back-end service url from env variables.
+     * We shouldn't prefix the env variable with NEXT_PUBLIC_* to avoid exposing it to the client.
+     */
+    target: process.env.PROMETHEUS_URL,
+    changeOrigin: true,
+    autoRewrite: false,
+  });
+}
