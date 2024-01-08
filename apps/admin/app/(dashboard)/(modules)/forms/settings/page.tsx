375a233a470f32e8e7668bdfa31a28a7fa6e2814
Konstantinos Feretos <konferetos@tutanota.com>
2024-01-08 12:09:20 +0200

    chore(admin): forms settings fc name

--- apps/admin/app/(dashboard)/(modules)/forms/settings/page.tsx
+++ apps/admin/app/(dashboard)/(modules)/forms/settings/page.tsx
@@ -1,7 +1,7 @@
 import { Settings } from '@/components/forms/settings';
 import { getFormsSettings } from '@/lib/api/forms';
 
-export default async function RouterSettings() {
+export default async function FormsSettings() {
   const { config: data } = await getFormsSettings()
   return <Settings data={data} />
-}
+};
