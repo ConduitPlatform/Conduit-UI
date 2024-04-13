296350de924160df5d64fe4370d4bef7b67440c3
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2024-04-13 19:33:16 +0300

    fix: loader issues refactor: axios init feat: authentication middleware

--- apps/admin/lib/api/api.ts
+++ apps/admin/lib/api/api.ts
@@ -1,30 +1,41 @@
-"use server";
-import axios, {AxiosInstance} from "axios";
-import {cookies} from "next/headers";
+'use server';
+import axios from 'axios';
+import { cookies } from 'next/headers';
 
-export const axiosInstance: AxiosInstance = (() => {
-  let instance: AxiosInstance;
-  instance = axios.create({
-    baseURL: process.env.API_BASE_URL,
-    withCredentials: true,
-    timeout: 50000, // request timeout in milliseconds
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      masterkey: process.env.MASTER_KEY,
-    },
-  });
-  instance.interceptors.request.use(
-    (config) => {
-      if (config.headers.Authorization) return config;
-      config.headers.Authorization = `Bearer ${
-        cookies().get("accessToken")?.value
-      }`;
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+export const axiosInstance = axios.create({
+  baseURL: process.env.API_BASE_URL,
+  withCredentials: true,
+  timeout: 50000, // request timeout in milliseconds
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    masterkey: process.env.MASTER_KEY,
+
+  },
+});
+axiosInstance.interceptors.request.use(
+  config => {
+    if (config.headers.Authorization) return config;
+    const accessToken = cookies().get('accessToken');
+    if (!accessToken) return config;
+    config.headers.Authorization = `Bearer ${accessToken.value}`;
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+axiosInstance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async error => {
+    const { response } = error;
+    if (response?.data?.message) {
+      console.error('error: ', response.status, response.data.message);
+    } else {
+      console.error('error: ', error.response.status, error.request.path);
     }
-  );
-  return instance;
-})();
+    return Promise.reject(error);
+  },
+);
---
+++ apps/admin/middleware.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const isOnLogin = (url: string) => url.endsWith('/login');
+
+// add function to check if the url ends in any file format like .ico, .png .json or any other file format using regex
+const isSpecialPath = (url: string) => {
+  const regex = /\/(middleware)\//;
+  return regex.test(url);
+};
+
+export async function middleware(request: NextRequest) {
+  if (isSpecialPath(request.url)) return NextResponse.next();
+  const cookieStore = request.cookies;
+  const accessToken = cookieStore.get('accessToken');
+  if (!accessToken) {
+    if (isOnLogin(request.url)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+  }
+  const res = await fetch(`${process.env.API_BASE_URL}/admins/me`, {
+    method: 'GET',
+    // @ts-ignore
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: `Bearer ${accessToken!.value}`,
+      masterkey: process.env.MASTER_KEY,
+    },
+  }).catch(() => null);
+  if (!res || res.status !== 200) {
+    if (request.method !== 'GET' && !isOnLogin(request.url)) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    } else if (isOnLogin(request.url)) {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL('/login', request.url));
+  } else {
+    if (isOnLogin(request.url)) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+    return NextResponse.next();
+
+  }
+}
+
+export const config = {
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
+};
