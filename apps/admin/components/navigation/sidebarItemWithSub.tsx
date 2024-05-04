04be52d5f35cda34bc7ea221a6c230b9481e2003
Konstantinos Kopanidis <kkopanidis@users.noreply.github.com>
2024-05-04 19:01:29 +0300

    fix: layout issues with sidebar activations

--- apps/admin/components/navigation/sidebarItemWithSub.tsx
+++ apps/admin/components/navigation/sidebarItemWithSub.tsx
@@ -1,28 +1,36 @@
 'use client';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import { ChevronRightIcon } from 'lucide-react';
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
 import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
-export const SidebarItemWithSub = ({ children }: { children?: ReactNode, }) => {
-  const [open, setOpen] = React.useState(false);
+export const SidebarItemWithSub = ({ children, active }: {
+  children?: ReactNode,
+  active: boolean
+}) => {
+  const [open, setOpen] = React.useState(active);
   const firstChild = React.Children.toArray(children)[0];
   const restChildren = React.Children.toArray(children).slice(1);
+  useEffect(() => {
+    setOpen(active);
+  }, [active]);
   return (
     <Collapsible
+      open={open}
       onOpenChange={(isOpen) => setOpen(isOpen)}
     >
-      <div className='flex items-center justify-between space-x-4'>
+      <div className={cn('flex items-center justify-between rounded-md hover:bg-accent', active && 'bg-accent')}>
         {firstChild}
         <CollapsibleTrigger asChild>
-          <Button variant='ghost' size='sm' className='w-9 p-0'>
+          <Button variant='ghost' size='sm' className='w-9 p-0 group-hover:bg-accent'>
             {open ? <ChevronRightIcon className='h-4 w-4 transform rotate-90' /> :
               <ChevronRightIcon className='h-4 w-4' />}
             <span className='sr-only'>Toggle</span>
           </Button>
         </CollapsibleTrigger>
       </div>
-      <CollapsibleContent className='space-y-2 pl-4'>
+      <CollapsibleContent className='pt-2 space-y-2 pl-4'>
         {restChildren}
       </CollapsibleContent>
     </Collapsible>
