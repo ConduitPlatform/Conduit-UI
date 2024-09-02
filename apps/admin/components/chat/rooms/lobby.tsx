4c9cb9153335170a20528d13ae2cff0540e71ae5
Rena Tsagka <cangaprogram@gmail.com>
2024-09-02 18:04:44 +0300

    feat(chat): create room

--- apps/admin/components/chat/rooms/lobby.tsx
+++ apps/admin/components/chat/rooms/lobby.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { CreateRoomForm } from '@/components/chat/rooms/createForm';
 
 type LobbyProps = {
   children?: React.ReactNode;
 };
 export const Lobby: React.FC<LobbyProps> = ({ children }) => {
-  return <>{children}</>;
+  const [open, setOpen] = useState<boolean>(false);
+  return (
+    <div className="flex flex-col space-y-5">
+      {children}
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button>Create Room</Button>
+        </DialogTrigger>
+        <DialogContent className={'max-w-fit'}>
+          <DialogHeader>
+            <DialogTitle>Create Room</DialogTitle>
+          </DialogHeader>
+          <CreateRoomForm callback={() => setOpen(!open)} />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 };
