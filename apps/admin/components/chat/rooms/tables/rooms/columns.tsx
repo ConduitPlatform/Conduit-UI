617d537cdcf664964a316358ec9abb68cf1a7fdd
Rena Tsagka <cangaprogram@gmail.com>
2024-09-05 17:54:19 +0300

    feat(chat): table delete functionality

--- apps/admin/components/chat/rooms/tables/rooms/columns.tsx
+++ apps/admin/components/chat/rooms/tables/rooms/columns.tsx
@@ -1,9 +1,5 @@
-'use client';
-
 import { ColumnDef } from '@tanstack/react-table';
 import { ChatRoom } from '@/lib/models/chat';
-import { Trash2Icon } from 'lucide-react';
-import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { DeleteRoomAlert } from '@/components/chat/rooms/delete';
 import { EyeIcon } from 'lucide-react';
@@ -29,11 +25,9 @@ export const columns: ColumnDef<ChatRoom, any>[] = [
     enableSorting: false,
     cell: props => {
       return !props.getValue() ? (
-        <button onClick={() => console.log('delete room')}>
-          <Trash2Icon className="w-4 h-4" />
-        </button>
+        <DeleteRoomAlert roomIds={[props.row.original._id]} />
       ) : (
-        <>del</>
+        <></>
       );
     },
   },
