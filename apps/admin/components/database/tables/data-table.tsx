ecf7afaafa2cd94f07558356d82f5206948c99f5
Rena Tsagka <57152951+Renc17@users.noreply.github.com>
2024-11-26 18:23:34 +0200

    feat(database): pagination explicit page/limit selectors (#274)

--- apps/admin/components/database/tables/data-table.tsx
+++ apps/admin/components/database/tables/data-table.tsx
@@ -15,11 +15,26 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { Button } from '@/components/ui/button';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
 import React, { useEffect, useState } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { createQueryString } from '@/lib/utils';
 import Decimal from 'decimal.js';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 export function DataTable({
   docs,
@@ -32,16 +47,19 @@ export function DataTable({
 }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
+  const [limit, setLimit] = useState(
+    searchParams.get('limit') ? Number(searchParams.get('limit')) : 20
+  );
   const [pagination, setPagination] = useState({
     pageIndex: Number(searchParams.get('pageIndex')) ?? 0,
-    pageSize: 19,
+    pageSize: limit,
   });
   const router = useRouter();
 
   useEffect(() => {
     setPagination({
       pageIndex: Number(searchParams.get('pageIndex')) ?? 0,
-      pageSize: 19,
+      pageSize: limit,
     });
   }, []);
 
@@ -58,13 +76,30 @@ export function DataTable({
     router.push(`${pathname}?${params}`);
   }, [pagination.pageIndex]);
 
+  useEffect(() => {
+    const params = createQueryString(
+      [
+        {
+          name: 'pageIndex',
+          value: '0',
+        },
+        {
+          name: 'limit',
+          value: limit.toString(),
+        },
+      ],
+      searchParams.toString()
+    );
+    router.push(`${pathname}?${params}`);
+  }, [limit]);
+
   const table = useReactTable({
     data: docs,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     onPaginationChange: setPagination,
-    pageCount: new Decimal(count).div(19).toNumber(),
+    pageCount: new Decimal(count).div(limit).toNumber(),
     manualPagination: true,
     manualSorting: true,
     getRowId: row => row._id,
@@ -72,6 +107,60 @@ export function DataTable({
       pagination,
     },
   });
+
+  const explicitPages: number[] = [];
+  for (
+    let p = table.getState().pagination.pageIndex - 1;
+    p < table.getPageCount() && explicitPages.length < 4;
+    p++
+  ) {
+    if (p < 2) {
+      continue;
+    }
+    explicitPages.push(p);
+  }
+
+  const pages = (
+    <div className="flex gap-x-3">
+      <PaginationItem>
+        <PaginationLink
+          className="cursor-pointer"
+          onClick={() => table.setPageIndex(0)}
+          isActive={0 === table.getState().pagination.pageIndex}
+        >
+          {1}
+        </PaginationLink>
+      </PaginationItem>
+      {pagination.pageIndex > 3 && <PaginationEllipsis />}
+      {explicitPages.map(p => (
+        <PaginationItem key={`page-${p}`}>
+          <PaginationLink
+            className="cursor-pointer"
+            isActive={table.getState().pagination.pageIndex === p - 1}
+            onClick={() => table.setPageIndex(p - 1)}
+          >
+            {new Decimal(p).ceil().toNumber()}
+          </PaginationLink>
+        </PaginationItem>
+      ))}
+      {pagination.pageIndex < table.getPageCount() - 3 && (
+        <PaginationEllipsis />
+      )}
+      {table.getPageCount() > 1 && (
+        <PaginationItem>
+          <PaginationLink
+            className="cursor-pointer"
+            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            isActive={
+              table.getPageCount() - 1 === table.getState().pagination.pageIndex
+            }
+          >
+            {new Decimal(table.getPageCount()).ceil().toNumber()}
+          </PaginationLink>
+        </PaginationItem>
+      )}
+    </div>
+  );
 
   return (
     <>
@@ -121,23 +210,40 @@ export function DataTable({
           )}
         </TableBody>
       </Table>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
+      <div className="flex items-center justify-center space-x-3 my-2">
+        <Pagination className="w-fit mx-0">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                className={`${!table.getCanPreviousPage() ? 'opacity-50 cursor-disabled' : 'cursor-pointer'}`}
+                onClick={() => table.previousPage()}
+              />
+            </PaginationItem>
+            <div className="my-2">{pages}</div>
+            <PaginationItem>
+              <PaginationNext
+                className={`${!table.getCanNextPage() ? 'opacity-50 cursor-disabled' : 'cursor-pointer'}`}
+                onClick={() => table.nextPage()}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+        <Select
+          onValueChange={value => {
+            setLimit(Number(value));
+          }}
+          value={limit.toString()}
+          defaultValue={'20'}
         >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
+          <SelectTrigger className="w-fit">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value={'10'}>Show 10</SelectItem>
+            <SelectItem value={'20'}>Show 20</SelectItem>
+            <SelectItem value={'40'}>Show 40</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
     </>
   );
